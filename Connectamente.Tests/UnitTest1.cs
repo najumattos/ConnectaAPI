a150303328da9a89176734dc1e0ad4ac100a6e0a
Ana Mattos <anajuliamattos02@gmail.com>
2026-02-14 09:50:12 -0300

    test: criando projeto de testes unitários

---
+++ Connectamente.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Connectamente.Tests;
+
+public class InicializacaoTests
+{
+    [Fact]
+    public void Teste_Deve_Passar_Para_Validar_O_Pipeline()
+    {
+        // Arrange (Preparação)
+        var valorEsperado = true;
+
+        // Act (Ação)
+        var valorAtual = true;
+
+        // Assert (Verificação)
+        Assert.Equal(valorEsperado, valorAtual);
+    }
+}
